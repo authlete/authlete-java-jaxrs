a63d12f1c5df036f818274706d098bad30cbb044
Justin Richer <justin.richer@authlete.com>
2018-05-01 15:33:23 -0400

    abstracted certificate extraction to base endpoint

--- src/main/java/com/authlete/jaxrs/BaseEndpoint.java
+++ src/main/java/com/authlete/jaxrs/BaseEndpoint.java
@@ -17,7 +17,15 @@
 package com.authlete.jaxrs;
 
 
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
+
+import org.apache.commons.codec.binary.Base64;
 
 
 /**
@@ -29,6 +37,20 @@ import javax.ws.rs.WebApplicationException;
  */
 public class BaseEndpoint
 {
+    /** 
+     * Headers to check for certificate path with proxy-forwarded certificate 
+     * information; the first entry is the client's certificate itself 
+     */
+    private String[] clientCertificatePathHeaders = {
+            "X-Ssl-Cert", // the client's certificate 
+            "X-Ssl-Cert-Chain-1", "X-Ssl-Cert-Chain-2", "X-Ssl-Cert-Chain-3", "X-Ssl-Cert-Chain-4" // the intermediate certificate path, not including the client's certificate or root
+    };
+    
+    /*
+     * Used for handling PEM format certificates.
+     */
+    private Base64 base64 = new Base64(Base64.PEM_CHUNK_SIZE, "\n".getBytes());
+    
     /**
      * Called when the internal request handler raises an exception.
      * The default implementation of this method calls {@code
@@ -42,4 +64,77 @@ public class BaseEndpoint
     {
         exception.printStackTrace();
     }
+
+    protected String[] extractClientCertificateChain(HttpServletRequest request)
+    {
+        // try to get the certificates from the servlet context directly
+        X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
+        
+        if (certs == null || certs.length == 0)
+        {
+            // we didn't find any certificates in the servlet request, try extracting them from the headers instead
+            List<String> headerCerts = new ArrayList<>();
+            
+            for (String headerName : clientCertificatePathHeaders)
+            {
+                String header = request.getHeader(headerName);
+                if (header != null)
+                {
+                    headerCerts.add(header);
+                }
+            }
+
+            if (headerCerts.isEmpty())
+            {
+                return null;
+            }
+            else
+            {
+                return headerCerts.toArray(new String[] {});
+            }
+        }
+        else 
+        {
+            String[] pemEncoded = new String[certs.length];
+            
+            try
+            {
+                for (int i = 0; i < certs.length; i++)
+                {
+                    // encode each certificate in PEM format
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("-----BEGIN CERTIFICATE-----\n");
+                    sb.append(base64.encode(certs[i].getEncoded()));
+                    sb.append("\n-----END CERTIFICATE-----\n");
+
+                    pemEncoded[i] = sb.toString();
+                    
+                }
+            } catch (CertificateEncodingException e)
+            {
+                // TODO What should be done with this error?
+                e.printStackTrace();
+                return null;
+            }
+            
+            return pemEncoded;
+            
+        }
+        
+    }
+    
+    protected String extractClientCertificate(HttpServletRequest request)
+    {
+        String[] certs = extractClientCertificateChain(request);
+        
+        if (certs != null && certs.length > 0)
+        {
+            return certs[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
 }
