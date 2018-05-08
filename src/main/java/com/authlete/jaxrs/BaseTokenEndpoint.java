42a4fb7c7e2d260632a6ec9c24b65c184d7d5121
Takahiko Kawasaki <taka@authlete.com>
2018-05-09 05:08:37 +0900

    Added a variant of handle() method to BaseTokenEndpoint for backward compatibility.

--- src/main/java/com/authlete/jaxrs/BaseTokenEndpoint.java
+++ src/main/java/com/authlete/jaxrs/BaseTokenEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Authlete, Inc.
+ * Copyright (C) 2016-2018 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,36 @@ public class BaseTokenEndpoint extends BaseEndpoint
     /**
      * Handle a token request.
      *
+     * This method is an alias of the {@link #handle(AuthleteApi,
+     * TokenRequestHandlerSpi, MultivaluedMap, String, String[]) handle()}
+     * method that accepts 5 arguments.
+     *
+     * @param api
+     *         An implementation of {@link AuthleteApi}.
+     *
+     * @param spi
+     *         An implementation of {@link TokenRequestHandlerSpi}.
+     *
+     * @param parameters
+     *         Request parameters of the token request.
+     *
+     * @param authorization
+     *         The value of {@code Authorization} header of the token request.
+     *
+     * @return
+     *         A response that should be returned to the client application.
+     */
+    public Response handle(
+            AuthleteApi api, TokenRequestHandlerSpi spi,
+            MultivaluedMap<String, String> parameters, String authorization)
+    {
+        return handle(api, spi, parameters, authorization, null);
+    }
+
+
+    /**
+     * Handle a token request.
+     *
      * <p>
      * This method internally creates a {@link TokenRequestHandler} instance and
      * calls its {@link TokenRequestHandler#handle(MultivaluedMap, String)} method.
@@ -53,10 +83,9 @@ public class BaseTokenEndpoint extends BaseEndpoint
      * When {@code TokenRequestHandler.handle()} method raises a {@link
      * WebApplicationException}, this method calls {@link #onError(WebApplicationException)
      * onError()} method with the exception. The default implementation of {@code onError()}
-     * calls {@code printStackTrace()} of the exception and does nothing else. You
-     * can override the method as necessary. After calling {@code onError()} method,
-     * this method calls {@code getResponse()} method of the exception and uses the
-     * returned value as a response from this method.
+     * does nothing. You can override the method as necessary. After calling
+     * {@code onError()} method, this method calls {@code getResponse()} method of
+     * the exception and uses the returned value as a response from this method.
      * </p>
      *
      * @param api
@@ -70,13 +99,15 @@ public class BaseTokenEndpoint extends BaseEndpoint
      *
      * @param authorization
      *         The value of {@code Authorization} header of the token request.
-     *         
+     *
      * @param clientCertificatePath
      *         The certificate path used in mutual TLS authentication, in PEM format. The
      *         client's own certificate is the first in this array. Can be {@code null}.
      *
      * @return
      *         A response that should be returned to the client application.
+     *
+     * @since 2.8
      */
     public Response handle(
             AuthleteApi api, TokenRequestHandlerSpi spi,
