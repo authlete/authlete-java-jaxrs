06e5411729f59b6323c7d6f0960efa275bc073b4
Justin Richer <justin.richer@authlete.com>
2018-05-25 11:42:22 -0400

    reject empty strings and "(null)" from incoming certificate headers

--- src/main/java/com/authlete/jaxrs/HeaderClientCertificateExtractor.java
+++ src/main/java/com/authlete/jaxrs/HeaderClientCertificateExtractor.java
@@ -29,6 +29,31 @@ import javax.servlet.http.HttpServletRequest;
  * clientCertificateChainHeaders} member list. The first element in the list is
  * header for the client's own certificate. Each additional header in the list
  * will be checked and added to the resulting output.
+ * 
+ * Headers that are missing, empty, or contain only the string {@code (null)}
+ * are not returned in the list.
+ * 
+ * Different proxy servers use different configuration methods. For the Apache
+ * server, one possible method using the default headers for this class is below:
+ * 
+ * <pre>{@code
+ *   SSLEngine on
+ *   SSLCertificateFile /etc/certs/tls.crt
+ *   SSLCertificateKeyFile /etc/certs/tls.key
+ *   SSLVerifyClient optional_no_ca
+ *   SSLOptions +StdEnvVars
+ *   RequestHeader set X-Ssl-Cipher "%{SSL_CIPHER}e" env=SSL_CIPHER
+ *   RequestHeader set X-Ssl-Cert "%{SSL_CLIENT_CERT}e" env=SSL_CLIENT_CERT
+ *   RequestHeader set X-Ssl-Protocol "%{SSL_PROTOCOL}e" env=SSL_PROTOCOL
+ *   RequestHeader set X-Ssl-Verify "%{SSL_CLIENT_VERIFY}e" env=SSL_CLIENT_VERIFY
+ *   RequestHeader set X-Ssl-Cert-Chain-1 "%{SSL_CLIENT_CERT_CHAIN_1}e" env=SSL_CLIENT_CERT_CHAIN_1
+ *   RequestHeader set X-Ssl-Cert-Chain-2 "%{SSL_CLIENT_CERT_CHAIN_2}e" env=SSL_CLIENT_CERT_CHAIN_2
+ *   RequestHeader set X-Ssl-Cert-Chain-3 "%{SSL_CLIENT_CERT_CHAIN_3}e" env=SSL_CLIENT_CERT_CHAIN_3
+ *   RequestHeader set X-Ssl-Cert-Chain-4 "%{SSL_CLIENT_CERT_CHAIN_4}e" env=SSL_CLIENT_CERT_CHAIN_4
+ *   ProxyPreserveHost on
+ *   ProxyPass "/" "http://localhost:8081/"
+ *   ProxyPassReverse "/" "http://localhost:8081/"
+ *}</pre>
  *
  * @author jricher
  *
@@ -60,7 +85,7 @@ public class HeaderClientCertificateExtractor implements ClientCertificateExtrac
         for (String headerName : getClientCertificateChainHeaders())
         {
             String header = request.getHeader(headerName);
-            if (header != null)
+            if (header != null && !header.isEmpty() && !header.equals("(null)")) // "(null)" is a value that misconfigured Apache servers will send instead of a missing header
             {
                 headerCerts.add(header);
             }
