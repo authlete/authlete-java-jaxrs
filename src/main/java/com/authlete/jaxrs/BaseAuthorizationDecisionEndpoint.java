4a6df032c494c4a1ce607119e3954d039f461fe2
Takahiko Kawasaki <taka@authlete.com>
2019-12-23 13:06:54 +0900

    Added 'handle(AuthleteApi, AuthorizationDecisionHandlerSpi, Params)' method to BaseAuthorizationDecisionEndpoint.

--- src/main/java/com/authlete/jaxrs/BaseAuthorizationDecisionEndpoint.java
+++ src/main/java/com/authlete/jaxrs/BaseAuthorizationDecisionEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Authlete, Inc.
+ * Copyright (C) 2016-2019 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ package com.authlete.jaxrs;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import com.authlete.common.api.AuthleteApi;
+import com.authlete.jaxrs.AuthorizationDecisionHandler.Params;
 import com.authlete.jaxrs.spi.AuthorizationDecisionHandlerSpi;
 
 
@@ -76,13 +77,60 @@ public class BaseAuthorizationDecisionEndpoint extends BaseEndpoint
             AuthleteApi api, AuthorizationDecisionHandlerSpi spi,
             String ticket, String[] claimNames, String[] claimLocales)
     {
+        Params params = new Params()
+                .setTicket(ticket)
+                .setClaimNames(claimNames)
+                .setClaimLocales(claimLocales)
+                ;
+
+        return handle(api, spi, params);
+    }
+
+
+    /**
+     * Handle an authorization decision request.
+     *
+     * <p>
+     * This method internally creates a {@link AuthorizationDecisionHandler} instance and
+     * calls its {@link AuthorizationDecisionHandler#handle(String, String[], String[])}
+     * method. Then, this method uses the value returned from the {@code handle()} method
+     * as a response from this method.
+     * </p>
+     *
+     * <p>
+     * When {@code AuthorizationDecisionHandler.handle()} method raises a {@link
+     * WebApplicationException}, this method calls {@link #onError(WebApplicationException)
+     * onError()} method with the exception. The default implementation of {@code onError()}
+     * calls {@code printStackTrace()} of the exception and does nothing else. You
+     * can override the method as necessary. After calling {@code onError()} method,
+     * this method calls {@code getResponse()} method of the exception and uses the
+     * returned value as a response from this method.
+     * </p>
+     *
+     * @param api
+     *         An implementation of {@link AuthleteApi}.
+     *
+     * @param spi
+     *         An implementation of {@link AuthorizationDecisionHandlerSpi}.
+     *
+     * @param params
+     *         Parameters necessary to handle the decision.
+     *
+     * @return
+     *         A response that should be returned to the client application.
+     *
+     * @since 2.26
+     */
+    public Response handle(
+            AuthleteApi api, AuthorizationDecisionHandlerSpi spi, Params params)
+    {
         try
         {
             // Create a handler.
             AuthorizationDecisionHandler handler = new AuthorizationDecisionHandler(api, spi);
 
             // Delegate the task to the handler.
-            return handler.handle(ticket, claimNames, claimLocales);
+            return handler.handle(params);
         }
         catch (WebApplicationException e)
         {
